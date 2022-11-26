103f0de7610d8d328cf2a75a8c5a5c5b21f5ab07
Hilora Dewett <h.dewett@outlook.com>
2022-11-25 19:01:46 -0500

    created fetchCoordsByIP function that provides coordinates for a given IP address

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-// fetchMyIP((error, ip) => {
+// const ip = fetchMyIP((error, ip) => {
 //   if (error) {
 //     console.log("It didn't work!" , error);
 //     return;
 //   }
 
 //   console.log('It worked! Returned IP:' , ip);
+//   return ip;
 // });
+
+fetchCoordsByIP("8.8.8.8",(error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned coordinates: ", coordinates);
+});
+
--- iss.js
+++ iss.js
@@ -1,29 +1,51 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
+
 const request = require("request");
-const fetchMyIP = function(callback) {
-  request("https://api.ipify.org/?format=json", function(error, response, body) {
+
+// const fetchMyIP = function(callback) {
+//   request("https://api.ipify.org/?format=json", function(error, response, body) {
+//     // error can be set if invalid domain, user is offline, etc.
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+//     // if non-200 status, assume server error
+//     if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     }
+
+//     let ip = JSON.parse(body).ip;
+//     callback(null, ip);
+//   });
+// };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, function(error, response, body) {
+
     // error can be set if invalid domain, user is offline, etc.
     if (error) {
       callback(error, null);
       return;
     }
-    // if non-200 status, assume server error
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+
+    //converting string into object
+    const parsedBody = JSON.parse(body);
+
+    console.log("parsedBody.success:  ",parsedBody.success);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
       return;
-    }
+    } 
 
-    let ip = JSON.parse(body).ip;
-    callback(null, ip);
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
+
   });
-};
+}
 
-module.exports = { fetchMyIP };
+
+module.exports = { fetchCoordsByIP };
